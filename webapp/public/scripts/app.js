420b93772796ff5f51efd2f89b27e4acf0cb6858
Juan G. Arias <juangarias@gmail.com>
2015-12-02 20:53:21 -0300

    Temporary remove angular-ui-route until it works.

--- webapp/public/scripts/app.js
+++ webapp/public/scripts/app.js
@@ -2,15 +2,4 @@
 'use strict';
 
 angular
-	.module('salesManager', ['ngResource', 'ui.bootstrap', 'ngAnimate', 'ui.router', 'chart.js'])
-	.config(function($stateProvider, $urlRouterProvider) {
-
-        $urlRouterProvider.otherwise('/contacts');
-        
-        $stateProvider
-            .state('contacts', {
-                url: '/contacts',
-                templateUrl: '/salesmanager/views/contacts.html',
-                controller: 'ContactsController as vm'
-            });
-    });
+	.module('salesManager', ['ngResource', 'ui.bootstrap', 'ngAnimate', 'chart.js']);
